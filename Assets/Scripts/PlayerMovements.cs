4466724a76c76a2feff395f2ec281ef1550ccf01
Raphaël CLERAULT <raphael.clerault@gmail.com>
2025-06-08 19:52:16 +0700

    heheheha

--- Assets/Scripts/PlayerMovements.cs
+++ Assets/Scripts/PlayerMovements.cs
@@ -1,58 +1,145 @@
 using UnityEngine;
+using Mirror;
 
-public class PlayerMovements : MonoBehaviour
+public class PlayerMovements : NetworkBehaviour
 {
-    public float speed = 5f; // Speed of the player movement
+    public float speed = 5f;
     public GameObject Camera;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject hitParticles;
+    private GameObject healthBar;
+    public int health = 100;
     void Start()
     {
-        // lock the cursor to the center of the screen and make it invisible
+        if (!isLocalPlayer)
+            return;
         Cursor.lockState = CursorLockMode.Locked;
 
         Cursor.visible = false;
-        
+
+        healthBar = GameObject.Find("HealthBar");
     }
 
-    // Update is called once per frame
+    void PlayParticules(Vector3 pos, Vector3 normal)
+    {
+        if (hitParticles != null)
+        {
+            GameObject particles = Instantiate(hitParticles, pos, Quaternion.identity);
+            particles.transform.rotation = Quaternion.LookRotation(normal);
+            particles.transform.position += normal * 0.1f;
+            Destroy(particles, 2f);
+        }
+    }
+
+    void DoDamage(int clientID)
+    {
+        health -= 10;
+        if (health <= 0)
+        {
+            Debug.Log("Player " + clientID + " has died.");
+        }
+        else
+        {
+            Debug.Log("Player " + clientID + " took damage. Health: " + health);
+        }
+        healthBar.transform.localScale = new Vector3((float)health / 100, 1, 1);
+    }
+
+    [ClientRpc]
+    void RpcShootPlayer(int clientID)
+    {
+        if (isLocalPlayer)
+        {
+            Debug.Log("Client " + clientID + " is shot.");
+            DoDamage(clientID);
+        }
+    }
+    
+    [ClientRpc]
+    void RpcShootParticles(Vector3 pos, Vector3 normal)
+    {
+        PlayParticules(pos, normal);
+    }
+
+    [Command(requiresAuthority = false)]
+    void CmdShootParticles(Vector3 pos, Vector3 normal)
+    {
+        RpcShootParticles(pos, normal);
+    }
+
+    [Command(requiresAuthority = false)]
+    void CmdShootPlayer(int clientID)
+    {
+        RpcShootPlayer(clientID);
+    }
+
+    void Shoot()
+    {
+        if (Physics.Raycast(Camera.transform.position, Camera.transform.forward, out RaycastHit hit, 100f))
+        {
+            CmdShootParticles(hit.point, hit.normal);
+            Debug.Log("Hit: " + hit.collider.name);
+            if (hit.collider.CompareTag("Player"))
+            {
+                int clientID = hit.collider.GetComponent<NetworkIdentity>().connectionToClient.connectionId;
+                Debug.Log("Shooting player with ID: " + clientID);
+                CmdShootPlayer(clientID);
+            }
+            else
+            {
+                Debug.Log("Hit something else: " + hit.collider.name);
+            }
+        }
+    }
+
+    float lastShootTime = 0f;
+    float shootInterval = 2f;
+
     void Update()
     {
-        // Get the horizontal and vertical input axes
+        if (!isLocalPlayer)
+            return;
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        // Create a new Vector3 for movement direction
         Vector3 movement = new Vector3(horizontal, 0, vertical);
 
-        // Normalize the movement vector to prevent faster diagonal movement
         if (movement.magnitude > 1)
         {
             movement.Normalize();
         }
 
-        // Move the player in the specified direction
         transform.Translate(movement * Time.deltaTime * speed);
+        //GetComponent<Rigidbody>().linearVelocity = movement * speed * Time.deltaTime;
 
-        // rotate the player with the mouse
         float mouseX = Input.GetAxis("Mouse X");
         float mouseY = Input.GetAxis("Mouse Y");
 
-        // Rotate the player around the Y axis based on mouse movement
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (GetComponent<Rigidbody>() != null && transform.position.y < 0.2f)
+            {
+                GetComponent<Rigidbody>().AddForce(Vector3.up * 5f, ForceMode.Impulse);
+            }
+        }
+
         transform.Rotate(0, mouseX * 5f, 0);
 
-        // Rotate the camera around the X axis based on mouse movement
         Camera.transform.Rotate(-mouseY * 5f, 0, 0);
 
-        // Clamp the camera rotation to prevent flipping
         Vector3 cameraRotation = Camera.transform.localEulerAngles;
         if (cameraRotation.x > 180)
         {
             cameraRotation.x -= 360;
         }
-        cameraRotation.x = Mathf.Clamp(cameraRotation.x, -60, 60); // Limit the camera rotation
+        cameraRotation.x = Mathf.Clamp(cameraRotation.x, -60, 60);
         Camera.transform.localEulerAngles = cameraRotation;
 
-
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Time.time - lastShootTime < shootInterval)
+                return;
+            lastShootTime = Time.time;
+            Shoot();
+        }
     }
 }
